93796c42266ba7bcccf6d15bb4823acd0c874956
grostbite <wencms@gmail.com>
2020-03-04 07:03:50 +0500

    Hello World
    This is an example application that displays "Hello World" on the screen.

---
+++ ru.example.app/app.hs
@@ -0,0 +1,19 @@
+/****************************************************
+
+         Development by wencms@gmail.com
+         
+         xFactureOS Volga: v1.1.3
+
+****************************************************/
+
+class Activity as App {
+
+    func main() {
+
+        App::setContentView("apps/ru.example.app/main.hell");
+
+    }
+
+}
+
+Activity::main();
